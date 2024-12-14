1ebc7ec5b7b8c1f9544d2044170e64e703f65146
Minke Zhang <minke.zhang@gmail.com>
2024-12-13 23:49:11 -0800

    Update Day 5

--- 2024/go/queue/main.go
+++ 2024/go/queue/main.go
@@ -12,7 +12,9 @@ const (
 	fn = "queue.txt"
 )
 
-func one(edges [][]int, seqs [][]int) int {
+func filter(edges [][]int, seqs [][]int, include bool) [][]int {
+	rseqs := [][]int{}
+
 	invalidPrev := map[int]map[int]bool{}
 	invalidNext := map[int]map[int]bool{}
 	for _, e := range edges {
@@ -55,11 +57,103 @@ func one(edges [][]int, seqs [][]int) int {
 
 		return true
 	}
+
+	for _, s := range seqs {
+		if v(s) == include {
+			rseqs = append(rseqs, s)
+		}
+	}
+
+	return rseqs
+}
+
+// two fixes a sequence to comply with the list of (H, T) pairs.
+//
+// The problem makes several assumptions which are not stated --
+//
+//  1. Only one valid sequence exists i.e.
+//     a. All numbers in a sequence shows up as an edge head or tail
+//     b. The set of relevant edges for each sequence must produce a totally
+//     ordered sequence (e.g. 1|2; 3|4 is not a valid set of edges for the
+//     sequence (1, 3, 2, 4).
+//  2. There are no cycles in the list of edges
+//
+// Given these assumptions, we know that the last element T of a valid sequence
+// will have no relevent edge E such that E = (X, T) for some X in the sequence.
+//
+// We then can remove this tail from the sequence and prune down the relevant
+// edges, then iterate over the next "new" tail.
+func two(edges [][]int, seqs [][]int) int {
+	seqs = filter(edges, seqs, false)
+
+	es := map[int]map[int]bool{}
+	for _, e := range edges {
+		h, t := e[0], e[1]
+
+		if _, ok := es[h]; !ok {
+			es[h] = map[int]bool{}
+		}
+		es[h][t] = true
+	}
+
 	acc := 0
 	for _, seq := range seqs {
-		if v(seq) {
-			acc += seq[(len(seq)-1)/2]
+		sl := map[int]bool{}
+		for _, i := range seq {
+			sl[i] = true
 		}
+
+		// Both the head and tail must exist in the edge lookup.
+		et := map[int]map[int]bool{}
+		for _, s := range seq {
+			et[s] = map[int]bool{}
+		}
+
+		for h, ts := range es {
+			if _, ok := sl[h]; ok {
+				for t := range ts {
+					if _, ok := sl[t]; ok {
+						et[h][t] = true
+					}
+				}
+			}
+		}
+
+		rseq := []int{}
+		for len(et) > 0 {
+			var tail int
+			for h, ts := range et {
+				if len(ts) == 0 { // Found current tail.
+					tail = h
+					delete(et, h)
+					break
+				}
+			}
+
+			// First element in t is the last element in the
+			// corrected sequence.
+			rseq = append(rseq, tail)
+
+			// Remove all references to the tail
+			for _, ts := range et {
+				if _, ok := ts[tail]; ok {
+					delete(ts, tail)
+				}
+			}
+		}
+		// Median element is invariant under reverse.
+		acc += rseq[(len(rseq)-1)/2]
+	}
+
+	return acc
+}
+
+func one(edges [][]int, seqs [][]int) int {
+	seqs = filter(edges, seqs, true)
+
+	acc := 0
+	for _, seq := range seqs {
+		acc += seq[(len(seq)-1)/2]
 	}
 	return acc
 }
@@ -104,4 +198,5 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("part 1: %v\n", one(edges, seq))
+	fmt.Printf("part 2: %v\n", two(edges, seq))
 }
--- 2024/go/queue/main_test.go
+++ 2024/go/queue/main_test.go
@@ -4,6 +4,63 @@ import (
 	"testing"
 )
 
+func TestTwo(t *testing.T) {
+	for _, c := range []struct {
+		name  string
+		edges [][]int
+		seqs  [][]int
+		want  int
+	}{
+		{
+			name:  "Trivial",
+			edges: [][]int{[]int{1, 2}, []int{2, 3}},
+			seqs:  [][]int{[]int{2, 1, 3}},
+			want:  2,
+		},
+		{
+			name: "Example",
+			edges: [][]int{
+				[]int{47, 53},
+				[]int{97, 13},
+				[]int{97, 61},
+				[]int{97, 47},
+				[]int{75, 29},
+				[]int{61, 13},
+				[]int{75, 53},
+				[]int{29, 13},
+				[]int{97, 29},
+				[]int{53, 29},
+				[]int{61, 53},
+				[]int{97, 53},
+				[]int{61, 29},
+				[]int{47, 13},
+				[]int{75, 47},
+				[]int{97, 75},
+				[]int{47, 61},
+				[]int{75, 61},
+				[]int{47, 29},
+				[]int{75, 13},
+				[]int{53, 13},
+			},
+			seqs: [][]int{
+				[]int{75, 47, 61, 53, 29},
+				[]int{97, 61, 53, 29, 13},
+				[]int{75, 29, 13},
+				[]int{75, 97, 47, 61, 53},
+				[]int{61, 13, 29},
+				[]int{97, 13, 75, 29, 47},
+			},
+			want: 123,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			if got := two(c.edges, c.seqs); got != c.want {
+				t.Errorf("two() = %v, want = %v", got, c.want)
+			}
+		})
+	}
+}
+
 func TestOne(t *testing.T) {
 	for _, c := range []struct {
 		name  string
