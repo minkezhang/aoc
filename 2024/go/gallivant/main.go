d7092429b985d6350bdcc449974f553d82002780
Minke Zhang <minke.zhang@gmail.com>
2024-12-14 01:47:26 -0800

    Update Day 6

--- 2024/go/gallivant/main.go
+++ 2024/go/gallivant/main.go
@@ -16,17 +16,92 @@ const (
 // TODO(minkezhang): Only add obstacle along the original path instead of
 // complete brute force.
 func two(data string) int {
+	l := len(strings.Split(data, "\n")[0])
+	p := path(data)
 	acc := 0
-	for i := 0; i < len(data); i++ {
-		if data[i] != '#' && data[i] != '^' && data[i] != '\n' {
-			test := []rune(data)
-			test[i] = '#'
-			if isLoop := one(string(test)) == -1; isLoop {
-				acc += 1
-			}
+	v := map[int]map[int]bool{}
+	for _, pos := range p[1:] { // Skip initial guard position.
+		if _, ok := v[pos[1]]; !ok {
+			v[pos[1]] = map[int]bool{}
+		}
+		if v[pos[1]][pos[0]] {
+			continue
+		}
+		v[pos[1]][pos[0]] = true
+		test := []rune(data)
+		test[pos[1]*(l+1)+pos[0]] = '#'
+		if isLoop := one(string(test)) == -1; isLoop {
+			acc += 1
 		}
 	}
 	return acc
+}
+
+func path(data string) [][]int {
+	p := [][]int{}
+	m := [][]bool{}         // m[X][Y] == true --> blocked
+	v := [][]map[int]bool{} // Track visited nodes.
+	var start []int
+	for y, line := range strings.Split(data, "\n") {
+		if len(line) == 0 {
+			continue
+		}
+		r := make([]bool, len(line))
+		for x, c := range line {
+			if c == '#' {
+				r[x] = true
+			}
+			if c == '^' {
+				start = []int{x, y}
+			}
+
+		}
+		m = append(m, r)
+		vr := make([]map[int]bool, len(line))
+		for i := range vr {
+			vr[i] = map[int]bool{}
+		}
+		v = append(v, vr)
+	}
+
+	direction := 0 // N E S W
+	v[start[1]][start[0]][direction] = true
+	l := len(m[0])
+	pos := start
+	for {
+		p = append(p, pos)
+
+		x, y := pos[0], pos[1]
+		var next []int
+		switch direction {
+		case 0:
+			next = []int{x, y - 1}
+		case 1:
+			next = []int{x + 1, y}
+		case 2:
+			next = []int{x, y + 1}
+		case 3:
+			next = []int{x - 1, y}
+		default:
+			panic("invalid direction")
+		}
+		x, y = next[0], next[1]
+		if x < 0 || x >= l || y < 0 || y >= len(m) { // Out of bounds.
+			break
+		}
+		if v[y][x][direction] { // Detected cycle.
+			return nil
+		}
+		if m[y][x] {
+			direction = (direction + 1) % 4
+		} else {
+			pos = next
+			if len(v[y][x]) == 0 { // Avoid double counting
+				v[y][x][direction] = true
+			}
+		}
+	}
+	return p
 }
 
 func one(data string) int {
