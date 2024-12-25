120f51912ab17f31360fbb4398367a4247254bad
Minke Zhang <minke.zhang@gmail.com>
2024-12-25 03:14:27 -0800

    Update Day 8

---
+++ 2024/go/collinearity/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	fn = "collinearity.txt"
+)
+
+type M struct {
+	dim       [2]int            // {x, y}
+	nodes     map[rune][][2]int // node -> [{x, y}]
+	antinodes map[[2]int]bool   // [x, y] -> true
+}
+
+func New(data []byte) *M {
+	lines := strings.Split(string(data), "\n")
+	height := 0
+
+	nodes := map[rune][][2]int{}
+	for y, l := range lines {
+		if l != "" {
+			height += 1
+			for x, n := range l {
+				if n != '.' {
+					nodes[n] = append(nodes[n], [2]int{x, y})
+				}
+			}
+		}
+	}
+
+	return &M{
+		dim:   [2]int{len(lines[0]), height},
+		nodes: nodes,
+	}
+}
+
+func (m *M) One() int {
+	m.antinodes = map[[2]int]bool{}
+	for _, nodes := range m.nodes {
+		for i, s := range nodes {
+			for _, t := range nodes[i+1:] {
+				d := [2]int{s[0] - t[0], s[1] - t[1]}
+				candidates := [][2]int{
+					[2]int{s[0] + d[0], s[1] + d[1]}, // s +  d
+					[2]int{t[0] - d[0], t[1] - d[1]}, // t + -d
+				}
+				for _, c := range candidates {
+					if c[0] >= 0 && c[0] < m.dim[0] && c[1] >= 0 && c[1] < m.dim[1] {
+						m.antinodes[c] = true
+					}
+				}
+			}
+		}
+	}
+	return len(m.antinodes)
+}
+
+func (m *M) Two() int {
+	m.antinodes = map[[2]int]bool{}
+	for _, nodes := range m.nodes {
+		for i, s := range nodes {
+			for _, t := range nodes[i+1:] {
+				d := [2]int{s[0] - t[0], s[1] - t[1]}
+
+				// s + d
+				c := s // Original antenna is also considered an antinode.
+				for c[0] >= 0 && c[0] < m.dim[0] && c[1] >= 0 && c[1] < m.dim[1] {
+					m.antinodes[c] = true
+					c = [2]int{c[0] + d[0], c[1] + d[1]}
+				}
+				// t + -d
+				c = t
+				for c[0] >= 0 && c[0] < m.dim[0] && c[1] >= 0 && c[1] < m.dim[1] {
+					m.antinodes[c] = true
+					c = [2]int{c[0] - d[0], c[1] - d[1]}
+				}
+			}
+		}
+	}
+	return len(m.antinodes)
+}
+
+func (m *M) String() string {
+	lines := [][]byte{}
+	for y := 0; y < m.dim[1]; y++ {
+		l := []byte{}
+		for x := 0; x < m.dim[0]; x++ {
+			l = append(l, byte('.'))
+		}
+		lines = append(lines, l)
+	}
+	for a := range m.antinodes {
+		lines[a[1]][a[0]] = byte('#')
+	}
+	for r, nodes := range m.nodes {
+		for _, n := range nodes {
+			if lines[n[1]][n[0]] == '#' {
+				lines[n[1]][n[0]] = byte('*')
+			} else {
+				lines[n[1]][n[0]] = byte(r)
+			}
+		}
+	}
+	elems := []string{}
+	for _, l := range lines {
+		elems = append(elems, string(l))
+	}
+	return strings.Join(elems, "\n")
+}
+
+func one(data []byte) int { return New(data).One() }
+func two(data []byte) int { return New(data).Two() }
+
+func read(fn string) ([]byte, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return io.ReadAll(f)
+}
+
+func main() {
+	data, err := read(fn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error opening file: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("part 1: %v\n", one(data))
+	fmt.Printf("part 1: %v\n", two(data))
+}
---
+++ 2024/go/collinearity/main_test.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestOne(t *testing.T) {
+	for _, c := range []struct {
+		name string
+		data []byte
+		want int
+	}{
+		{
+			name: "Simple",
+			data: []byte(`.`),
+			want: 0,
+		},
+		{
+			name: "Simple/Horizontal",
+			data: []byte(`a.a..`),
+			want: 1,
+		},
+		{
+			name: "Simple/Horizontal/Reverse",
+			data: []byte(`..a.a`),
+			want: 1,
+		},
+		{
+			name: "Simple/Horizontal/Overlap",
+			data: []byte(`a.a.a`),
+			want: 2,
+		},
+		{
+			name: "DifferentFreq",
+			data: []byte(`A.a..`),
+			want: 0,
+		},
+		{
+			name: "Diagonal",
+			data: []byte(`.....
+.....
+.aA..
+..aA.
+.....
+`),
+			want: 4,
+		},
+		{
+			name: "Example",
+			data: []byte(`............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+`),
+			want: 14,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			if got := New(c.data).One(); got != c.want {
+				t.Errorf("One() = %v, want = %v", got, c.want)
+			}
+		})
+	}
+}
+
+func TestTwo(t *testing.T) {
+	for _, c := range []struct {
+		name string
+		data []byte
+		want int
+	}{
+		{
+			name: "Simple",
+			data: []byte(`.`),
+			want: 0,
+		},
+		{
+			name: "Simple/Horizontal",
+			data: []byte(`a.a....`),
+			want: 4,
+		},
+		{
+			name: "Example",
+			data: []byte(`............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+`),
+			want: 34,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			if got := New(c.data).Two(); got != c.want {
+				t.Errorf("Two() = %v, want = %v", got, c.want)
+			}
+		})
+	}
+}
