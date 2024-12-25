2d0b229c5a26e403a8f7dfbf12dff3c0b00f68c4
Minke Zhang <minke.zhang@gmail.com>
2024-12-25 01:36:55 -0800

    Update Day 7

--- 2024/go/bridge/main.go
+++ 2024/go/bridge/main.go
@@ -12,6 +12,62 @@ const (
 	fn = "bridge.txt"
 )
 
+// checkConcat if a target T may be made by two values X, Y s.t. T == X || Y
+//
+// Cases:
+//
+//	T: 12, Y: 2  --> Success
+//	T: 12, Y: 1  --> Fail
+//	T: 12, Y: 12 --> Success
+//
+// T: 12, Y: 12 reduces to T: 1, Y: 1 i.e. mod 10 and compare values.
+func checkConcat(target int, x int) (int, bool) {
+	if target == 0 && x != 0 {
+		return 0, false
+	}
+	if target%10 == x%10 {
+		if x < 10 {
+			return target / 10, true
+		} else {
+			return checkConcat(target/10, x/10)
+		}
+	}
+	return 0, false
+}
+
+func checkTwo(target int, xs []int) bool {
+	if len(xs) == 0 {
+		return false
+	}
+	if len(xs) == 1 {
+		return target == xs[0]
+	}
+	x := xs[len(xs)-1]
+	res, succ := checkConcat(target, x)
+	return ((target >= x && checkTwo(target-x, xs[:len(xs)-1])) || (target%x == 0 && checkTwo(target/x, xs[:len(xs)-1]))) || (succ && checkTwo(res, xs[:len(xs)-1]))
+}
+
+func two(data []byte) int {
+	acc := 0
+	for _, l := range strings.Split(string(data), "\n") {
+		if l == "" {
+			continue
+		}
+		ps := strings.SplitN(l, ":", 2)
+		qs := []string{ps[0]}
+		qs = append(qs, strings.Split(strings.Trim(ps[1], " "), " ")...)
+		xs := []int{}
+		for _, p := range qs {
+			x, _ := strconv.Atoi(p)
+			xs = append(xs, x)
+		}
+		if checkTwo(xs[0], xs[1:]) {
+			acc += xs[0]
+		}
+	}
+	return acc
+}
+
 func check(target int, xs []int) bool {
 	if len(xs) == 0 {
 		return false
@@ -64,4 +120,5 @@ func main() {
 	}
 
 	fmt.Printf("part 1: %v\n", one(data))
+	fmt.Printf("part 2: %v\n", two(data))
 }
--- 2024/go/bridge/main_test.go
+++ 2024/go/bridge/main_test.go
@@ -4,6 +4,117 @@ import (
 	"testing"
 )
 
+func TestCheckConcat(t *testing.T) {
+	for _, c := range []struct {
+		name   string
+		target int
+		x      int
+		res    int
+		want   bool
+	}{
+		{
+			name:   "Trivial",
+			target: 0,
+			x:      0,
+			res:    0,
+			want:   true,
+		},
+		{
+			name:   "Trivial/False",
+			target: 1,
+			x:      4,
+			res:    0,
+			want:   false,
+		},
+		{
+			name:   "Simple/T=12/X=2",
+			target: 12,
+			x:      2,
+			res:    1,
+			want:   true,
+		},
+		{
+			name:   "Simple/T=12/X=1",
+			target: 12,
+			x:      1,
+			res:    0,
+			want:   false,
+		},
+		{
+			name:   "Simple/T=12/X=12",
+			target: 12,
+			x:      12,
+			res:    0,
+			want:   true,
+		},
+		{
+			name:   "Simple/T=12000/X=12",
+			target: 12000,
+			x:      12,
+			res:    0,
+			want:   false,
+		},
+		{
+			name:   "Simple/T=12000/X=0",
+			target: 12000,
+			x:      0,
+			res:    1200,
+			want:   true,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			if result, got := checkConcat(c.target, c.x); result != c.res || got != c.want {
+				t.Errorf("checkConcat() = %v, %v want = %v, %v", result, got, c.res, c.want)
+			}
+		})
+	}
+}
+
+func TestTwo(t *testing.T) {
+	for _, c := range []struct {
+		name string
+		data []byte
+		want int
+	}{
+		{
+			name: "Simple",
+			data: []byte(`156: 15 6`),
+			want: 156,
+		},
+		{
+			name: "Simple/Two",
+			data: []byte(`7290: 6 8 6 15`),
+			want: 7290,
+		},
+		{
+			name: "Simple/Three",
+			data: []byte(`192: 17 8 14`),
+			want: 192,
+		},
+		{
+			name: "Example",
+			data: []byte(`190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`),
+			want: 11387,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			if got := two(c.data); got != c.want {
+				t.Errorf("two() = %v, want = %v", got, c.want)
+			}
+
+		})
+	}
+}
+
 func TestOne(t *testing.T) {
 	for _, c := range []struct {
 		name string
